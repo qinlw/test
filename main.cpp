5e926026ec82060dda3641b5c131559e60b77554
2162036628@qq.com <2162036628@qq.com>
2025-04-09 16:15:02 +0800

    [0] TBranch1提交

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream> 
+using namespace std;
 
 void fun() {
 	printf("hello word");
 }
 
+void fun(int x) {
+	cout << x << endl;
+}
+
 int main() {
 	printf("hello word");
 	fun();
+	fun(3);
 
 	return 0;
 }
